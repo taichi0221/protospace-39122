782b1f8e61d780803c02f3128a4ba531bd01e075
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-24 09:59:20 +0900

    destroy追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @prototypes = Prototype.all
